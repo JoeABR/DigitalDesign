8c725fa49faf7155031aab3d7e3ac5592d8addb3
JoeABR <156911846+JoeABR@users.noreply.github.com>
2025-09-06 16:50:33 +0300

    code documentation

--- Comb/Arithmetic/Atomic/fl-adder/VHDL/Dataflow/full_adder.vhd
+++ Comb/Arithmetic/Atomic/fl-adder/VHDL/Dataflow/full_adder.vhd
@@ -16,16 +16,18 @@ entity full_adder is
 		augend :IN std_logic; --! the current value
 		addend :IN std_logic; --! the value added to current
 		carry_in : IN std_logic; --! the carry in from previous addition if present
-		sum :OUT std_logic --! the sum of the addition
+		sum : OUT std_logic; --! the sum of the addition
 		carry_out : OUT std_logic --! the carry out from the addition if generated
 	);
 
 end full_adder;
-
+--! Description of the architecture used
 architecture dataflow of full_adder is
 
+--! Core implementation of the architecture
 begin 
-	sum <= augend xor addend xor carry_in;
+	sum <= augend xor addend xor carry_in; --! Sum signal as implemented by a boolean expression
 
-	carry_out <= (augend and addend) or (augend and carry_in) or (addend and carry_in);
+	carry_out <= (augend and addend) or (augend and carry_in) or (addend and carry_in); --! Carry-out signal as implemented by a boolean expression
 end dataflow;
+--! End of the implementation
